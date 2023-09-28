0328d25a1ec608ea178f8d731626bf4906c099ad
crlsktr <crlsktr@gmail.com>
2023-09-28 15:45:38 -0600

    game loop and basic score

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,54 @@
     import {animalOptions} from "$lib/animals";
     import MayanNumeral from "$lib/components/MayanNumeral.svelte";
 
-    let selected_animal_index :number = Math.floor(Math.random() * animalOptions.length);
-    let selected_number_index :number = Math.floor(Math.random() * numerals.length);
-    let src = animalOptions[selected_animal_index] + ".png";
-    let possible_options = [
-        numberNames[selected_number_index],
-        get_other_random(numberNames[selected_number_index],numberNames),
-        get_other_random(numberNames[selected_number_index],numberNames)
-    ];
-    shuffleArray(possible_options);
+    let selected_animal_index :number = 0;
+    let selected_number_index :number = 0;
+    let src:string = "";
+    let possible_options:string[] = [];
     let latest_answer:string = "";
-    const checkAnswer = (ans:string) => {latest_answer = ans;}
+    let tries:number = 0;
+    let correct: number = 0;
+
+    const resetGame = () => {
+        selected_animal_index= Math.floor(Math.random() * animalOptions.length);
+        selected_number_index= Math.floor(Math.random() * numerals.length);
+        src = animalOptions[selected_animal_index] + ".png";
+        possible_options = [
+            numberNames[selected_number_index],
+            get_other_random(numberNames[selected_number_index],numberNames),
+            get_other_random(numberNames[selected_number_index],numberNames)
+        ];
+        shuffleArray(possible_options);
+        latest_answer = "";
+    }
+    resetGame();
+    const checkAnswer = (ans:string) => {
+        if (latest_answer!==""){
+            latest_answer = ans;
+            return;
+        }
+        tries++;
+        ans === numberNames[selected_number_index] ? correct++:correct;
+        latest_answer = ans;
+    }
 </script>
 <style>
+    @font-face {
+        font-family: 'material-ui';
+        src: url('/MaterialSymbolsOutlined.ttf')
+    }
     img {
         width: 10%;
+    }
+    .material-symbols-outlined {
+        font-variation-settings:
+        'FILL' 0,
+        'wght' 400,
+        'GRAD' 0,
+        'opsz' 24
+    }
+    span {
+        font-family: 'material-ui';
     }
 </style>
 {#each Array(selected_number_index + 1) as img,i}
@@ -27,10 +60,16 @@
     {/if}
 {/each}
 <MayanNumeral number="{selected_number_index}" />
-<!-- <h2>{numerals[selected_number_index]}</h2> -->
 <h2>{selected_number_index}</h2>
-
-{#each possible_options as possible}
+<div>
+    {#each possible_options as possible}
     <button on:click={() => checkAnswer(possible)}>{possible}</button>
-{/each}
-{#if latest_answer !=="" }<h2>{latest_answer === numberNames[selected_number_index] ? "✔️" : "❌"}</h2>{/if}
+    {/each}
+</div>
+{#if latest_answer !=="" }
+    <h2>{latest_answer === numberNames[selected_number_index] ? "✔️" : "❌"}</h2>
+    <button on:click={() => resetGame()}>
+        <span class="material-symbols-outlined">restart_alt</span>
+    </button>
+{/if}
+<div>{correct}/{tries}</div>
