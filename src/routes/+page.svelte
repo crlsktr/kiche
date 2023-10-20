b41a04fb306ebe7f1b2a7b53523df9a24e0f9478
crlsktr <crlsktr@gmail.com>
2023-10-20 15:10:39 -0600

    remove code hints from demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Skeleton.</h2>
+		<h2 class="h2">K'iche' etz'anem</h2>
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
@@ -28,12 +28,6 @@
 				<li><a href="{base}/ajilabal"><span class="flex-auto">Ajilab'al</span></a></li>
 			</ul>
 		</nav>
-		
-		<div class="space-y-2">
-			<p>Try editing the following:</p>
-			<p><code class="code">/src/routes/+layout.svelte</code></p>
-			<p><code class="code">/src/routes/+page.svelte</code></p>
-		</div>d
 	</div>
 </div>
 
