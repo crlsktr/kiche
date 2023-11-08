1da2512fd477d14dbdf5505fc51f0e6edcdc4348
crlsktr <crlsktr@gmail.com>
2023-11-08 14:06:42 -0700

    add placeholder page for css classes applied dynamically

---
+++ src/routes/placeholder/+page.svelte
@@ -0,0 +1,5 @@
+<button class="btn btn-m variant-ghost-primary"></button>
+<button class="btn btn-m variant-filled-secondary"></button>
+<button class="btn btn-m variant-filled-tertiary"></button>
+<button class="btn btn-m variant-filled-success"></button>
+<button class="btn btn-m variant-filled-failure"></button>
